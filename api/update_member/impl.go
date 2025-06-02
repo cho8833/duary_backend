0dbec6f7769dd561842b983cc854173c5ff8210f
cho8833 <chowsp1009@gmail.com>
2025-06-02 20:05:28 +0900

    update_member.UpdateMemberReq - removed AlarmOffset fields

--- api/update_member/impl.go
+++ api/update_member/impl.go
@@ -3,7 +3,6 @@ package update_member
 import (
 	"github.com/cho8833/duary_lambda/model"
 	"github.com/cho8833/duary_lambda/model/couple"
-	"github.com/cho8833/duary_lambda/model/event"
 	"github.com/cho8833/duary_lambda/model/member"
 	"github.com/cho8833/duary_lambda/shared"
 	"log"
@@ -11,12 +10,10 @@ import (
 )
 
 type UpdateMemberReq struct {
-	Birthday   *time.Time        `json:"birthday"`
-	Name       *string           `json:"name"`
-	Character  *string           `json:"character"`
-	FcmToken   *string           `json:"fcmToken"`
-	LoverAlarm event.AlarmOffset `json:"loverAlarm"`
-	MyAlarm    event.AlarmOffset `json:"myAlarm"`
+	Birthday  *time.Time `json:"birthday"`
+	Name      *string    `json:"name"`
+	Character *string    `json:"character"`
+	FcmToken  *string    `json:"fcmToken"`
 }
 
 type UpdateMemberRes struct {
@@ -43,14 +40,12 @@ func UpdateMember(req *UpdateMemberReq, transaction *model.DynamoDBWriteTransact
 
 	// update member
 	memberUpdateReq := &member.UpdateMemberReq{
-		SocialId:   *socialId,
-		Provider:   *provider,
-		Name:       req.Name,
-		Birthday:   req.Birthday,
-		Character:  req.Character,
-		FcmToken:   req.FcmToken,
-		LoverAlarm: req.LoverAlarm,
-		MyAlarm:    req.MyAlarm,
+		SocialId:  *socialId,
+		Provider:  *provider,
+		Name:      req.Name,
+		Birthday:  req.Birthday,
+		Character: req.Character,
+		FcmToken:  req.FcmToken,
 	}
 	updatedMember, svcErr := memberSvc.UpdateMemberTransaction(memberUpdateReq, transaction)
 	if svcErr != nil {
