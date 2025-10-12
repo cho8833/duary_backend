b1183589614ce3831ffd6d16fb8d7f3d10f9625b
cho8833 <chowsp1009@gmail.com>
2025-10-12 19:05:22 +0900

    dev dynamodb iam policies

--- terraform/environments/dev/iam_policies.tf
+++ terraform/environments/dev/iam_policies.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "dynamodb_dev_event_crud_policy" {
+data "aws_iam_policy_document" "dynamodb_dev_event_crud_policy_doc" {
     statement {
         sid = "AllowCRUDDevEventTable"
         effect = "Allow"
@@ -14,11 +14,145 @@ data "aws_iam_policy_document" "dynamodb_dev_event_crud_policy" {
             [module.dev_event_table.arn],
             values(module.dev_event_table.lsi_arns)
         )
+
+        condition {
+            test     = "StringEquals"
+            values = ["dev"]
+            variable = "apigateway:stage"
+        }
     }
     version = "2012-10-17"
 }
 
-resource "aws_iam_policy" "dynamodb_event_crud_policy" {
+resource "aws_iam_policy" "dynamodb_dev_event_crud_policy" {
     name = "dev-dynamodb-event-crud-policy"
-    policy = data.aws_iam_policy_document.dynamodb_dev_event_crud_policy.json
+    policy = data.aws_iam_policy_document.dynamodb_dev_event_crud_policy_doc.json
+}
+
+data "aws_iam_policy_document" "dynamodb_dev_cert_crud_policy_doc" {
+    statement {
+        sid = "AllowCRUDDevCertTable"
+        effect = "Allow"
+        actions = [
+            "dynamodb:GetItem",
+            "dynamodb:Scan",
+            "dynamodb:Query",
+            "dynamodb:PutItem",
+            "dynamodb:UpdateItem",
+            "dynamodb:DeleteItem"
+        ]
+
+        resources = [
+            module.dev_cert_table.arn
+        ]
+
+        condition {
+            test     = "StringEquals"
+            values = ["dev"]
+            variable = "apigateway:stage"
+        }
+    }
+
+    version = "2012-10-17"
+}
+
+resource "aws_iam_policy" "dynamodb_dev_cert_crud_policy" {
+    name = "dev-dynamodb-cert-crud-policy"
+    policy = data.aws_iam_policy_document.dynamodb_dev_cert_crud_policy_doc.json
+}
+
+data "aws_iam_policy_document" "dynamodb_dev_couple_crud_policy_doc" {
+    statement {
+        sid = "AllowCRUDDevCoupleTable"
+        effect = "Allow"
+        actions = [
+            "dynamodb:GetItem",
+            "dynamodb:Scan",
+            "dynamodb:Query",
+            "dynamodb:PutItem",
+            "dynamodb:UpdateItem",
+            "dynamodb:DeleteItem"
+        ]
+
+        resources = [
+            module.dev_couple_table.arn
+        ]
+
+        condition {
+            test     = "StringEquals"
+            values = ["dev"]
+            variable = "apigateway:stage"
+        }
+    }
+
+    version = "2012-10-17"
+}
+
+resource "aws_iam_policy" "dynamodb_dev_couple_crud_policy" {
+    name = "dev-dynamodb-couple-crud-policy"
+    policy = data.aws_iam_policy_document.dynamodb_dev_couple_crud_policy_doc.json
+}
+
+data "aws_iam_policy_document" "dynamodb_dev_member_crud_policy_doc" {
+    statement {
+        sid = "AllowCRUDDevMemberTable"
+        effect = "Allow"
+        actions = [
+            "dynamodb:GetItem",
+            "dynamodb:Scan",
+            "dynamodb:Query",
+            "dynamodb:PutItem",
+            "dynamodb:UpdateItem",
+            "dynamodb:DeleteItem"
+        ]
+
+        resources = [
+            module.dev_member_table.arn
+        ]
+
+        condition {
+            test     = "StringEquals"
+            values = ["dev"]
+            variable = "apigateway:stage"
+        }
+    }
+
+    version = "2012-10-17"
+}
+
+resource "aws_iam_policy" "dynamodb_dev_member_crud_policy" {
+    name = "dev-dynamodb-member-crud-policy"
+    policy = data.aws_iam_policy_document.dynamodb_dev_member_crud_policy_doc.json
+}
+
+data "aws_iam_policy_document" "dynamodb_dev_ws_connection_crud_policy_doc" {
+    statement {
+        sid = "AllowCRUDDevWSConnectionTable"
+        effect = "Allow"
+        actions = [
+            "dynamodb:GetItem",
+            "dynamodb:Scan",
+            "dynamodb:Query",
+            "dynamodb:PutItem",
+            "dynamodb:UpdateItem",
+            "dynamodb:DeleteItem"
+        ]
+
+        resources = [
+            module.dev_ws_conneciton_table.arn
+        ]
+
+        condition {
+            test     = "StringEquals"
+            values = ["dev"]
+            variable = "apigateway:stage"
+        }
+    }
+
+    version = "2012-10-17"
+}
+
+resource "aws_iam_policy" "dynamodb_dev_ws_connection_crud_policy" {
+    name = "dev-dynamodb-ws-connection-crud-policy"
+    policy = data.aws_iam_policy_document.dynamodb_dev_ws_connection_crud_policy_doc.json
 }
